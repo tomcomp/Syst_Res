be1f160ce43688185c51118fb8a84bbfb28cce55
HDieTox <tom.compagnon2003@gmail.com>
2025-10-07 10:32:00 +0200

    2. Add to free List

--- TP2/alloc.c
+++ TP2/alloc.c
@@ -7,37 +7,24 @@ typedef struct HEADER_TAG {
     long magic_number; /* 0x0123456789ABCDEFL */
 } HEADER;
 
-HEADER* init_list() {
-    return NULL;
-}
+HEADER* create_block(size_t size) {
+    size_t total_size = sizeof(HEADER) + size + sizeof(long);
+    HEADER* header = sbrk(total_size);
+    if (header == (void*)-1) {
+        return NULL;
+    }
 
-void create_block(HEADER* block, size_t size) {
-    block->ptr_next = NULL;
-    block->bloc_size = size;
-    block->magic_number = 0x0123456789ABCDEFL;
+    header->ptr_next = NULL;
+    header->bloc_size = size;
+    header->magic_number = 0x0123456789ABCDEFL;
+    long* footer = (long*) (((void*)header) + sizeof(HEADER) + size);
+    *footer = 0x0123456789ABCDEFL;
+    return header;
 }
 
 void insert_block(HEADER** head, HEADER* new_block) {
     new_block->ptr_next = *head;
     *head = new_block;
-}
-
-HEADER* pop_block(HEADER** head, HEADER* block_to_pop) {
-    HEADER* curr = *head;
-    HEADER* prev = NULL;
-    while (curr) {
-        if (curr == block_to_pop) {
-            if (prev) {
-                prev->ptr_next = curr->ptr_next;
-            } else {
-                *head = curr->ptr_next;
-            }
-            return curr;
-        }
-        prev = curr;
-        curr = curr->ptr_next;
-    }
-    return NULL;
 }
 
 void print_list(HEADER* head) {
@@ -66,17 +53,27 @@ void* malloc_3is(size_t size) {
 
 
 int main() {
+
+    HEADER* free_list = NULL;
+
     void* ptr1 = malloc_3is(1<<8);
-    void* ptr2 = malloc_3is(1<<10);
-    void* ptr3 = malloc_3is(1<<12);
+    void* ptr2 = malloc_3is(1<<9);
+    void* ptr3 = malloc_3is(1<<10);
 
     HEADER* block1 = (void*)ptr1 - sizeof(HEADER);
     HEADER* block2 = (void*)ptr2 - sizeof(HEADER);
     HEADER* block3 = (void*)ptr3 - sizeof(HEADER);
 
+    printf("Allocated blocks:\n");
     printf("Block1 at %p, size: %zu, magic: %lx\n", (void*)block1, block1->bloc_size, block1->magic_number);
     printf("Block2 at %p, size: %zu, magic: %lx\n", (void*)block2, block2->bloc_size, block2->magic_number);
     printf("Block3 at %p, size: %zu, magic: %lx\n", (void*)block3, block3->bloc_size, block3->magic_number);
+
+    insert_block(&free_list, block1);
+    insert_block(&free_list, block2);
+
+    printf("\nFree list after inserting block1 and block2:\n");
+    print_list(free_list);
 
     return 0;
 }
